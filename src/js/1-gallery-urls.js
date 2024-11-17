acf2dd7bec081ebea63031784cf44617f5c822bc
BartoszZygmunt <it@bartoszzygmunt.pl>
2024-11-17 21:41:32 +0100

    path with js v2

--- src/js/1-gallery-urls.js
+++ src/js/1-gallery-urls.js
@@ -2,7 +2,18 @@
 // We will use these URLs in the next file to load the images.
 
 // Define the URLs for the gallery images
-for (let i = 1; i <= 9; i++) {
-    const imgUrl = new URL(`../img/image${i}.png`, import.meta.url).href;
-    document.getElementById(`img${i}`).href = imgUrl;
-}
+
+// Tutaj wyszukiwanie selektorów po ID - nasz Trener mówi, że no takie nie bardzo profi ;)
+// for (let i = 1; i <= 9; i++) {
+//     const imgUrl = new URL(`../img/image${i}.png`, import.meta.url).href;
+//     document.getElementById(`img${i}`).href = imgUrl;
+// }
+
+// Tutaj mam nadzieję, że już bardziej profesjonalnie ;)
+// Pobierz wszystkie elementy <a> z klasą 'image-link'
+const anchors = document.querySelectorAll('a.gallery-link');
+
+anchors.forEach((anchor, index) => {
+  const imgUrl = new URL(`../img/image${index + 1}.png`, import.meta.url).href;
+  anchor.href = imgUrl;
+});
