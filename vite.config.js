bbffd2fde12a48f48dc2ec873c5e00d6f3d88bee
BartoszZygmunt <it@bartoszzygmunt.pl>
2024-11-17 19:37:42 +0100

    p12

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-advancedjs-hw-01/',
     server: {
       https: false,
     },
